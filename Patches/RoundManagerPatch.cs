6e98583386dee9ea066274cf078552fb2601114a
Xu Xiaolan <126674348+XuuXiaolan@users.noreply.github.com>
2025-06-24 12:07:00 +0100

    Update RoundManagerPatch.cs to use System.Random and System.Random extensions

--- Patches/RoundManagerPatch.cs
+++ Patches/RoundManagerPatch.cs
@@ -2,40 +2,38 @@ using HarmonyLib;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
-namespace InteriorFogConfig.Patches
+namespace InteriorFogConfig.Patches;
+[HarmonyPatch(typeof(RoundManager))]
+public class RoundManagerPatch
 {
-    [HarmonyPatch(typeof(RoundManager))]
-    public class RoundManagerPatch
+    [HarmonyPatch("RefreshEnemiesList")]
+    [HarmonyPostfix]
+    private static void ApplyNewFogValues(RoundManager __instance)
     {
-        [HarmonyPatch("RefreshEnemiesList")]
-        [HarmonyPostfix]
-        private static void ApplyNewFogValues(RoundManager __instance)
+        if (__instance.indoorFog == null)
         {
-            InteriorFogConfig.Logger.LogInfo("Running Indoor Fog changes.");
-            if (__instance.indoorFog != null)
-            {
-                if (InteriorFogConfig.randomFogThinness.Value)
-                {
-                    float randomThinness = Random.Range(InteriorFogConfig.minThinness.Value, InteriorFogConfig.maxThinness.Value);
-                    InteriorFogConfig.Logger.LogInfo("Random fog thinness value: " + randomThinness);
-                    __instance.indoorFog.parameters.meanFreePath = randomThinness;
-                }
-                else
-                {
-                    __instance.indoorFog.parameters.meanFreePath = InteriorFogConfig.fogThinness.Value;
-                }
-                
-                if (InteriorFogConfig.changeFogColor.Value)
-                {
-                    ColorUtility.TryParseHtmlString(InteriorFogConfig.fogColor.Value, out Color color);
-                    __instance.indoorFog.parameters.albedo = color;
-                }
-                InteriorFogConfig.Logger.LogInfo("Indoor fog changes done.");
-            }
-            else
-            {
-                InteriorFogConfig.Logger.LogWarning("Indoor fog changes were not done. indoorFog object is null.");
-            }
+            return;
         }
+        if (InteriorFogConfig.randomFogThinness.Value)
+        {
+            System.Random fogRandom = new(StartOfRound.Instance.randomMapSeed + 69);
+            float randomThinness = fogRandom.NextFloat(InteriorFogConfig.minThinness.Value, InteriorFogConfig.maxThinness.Value);
+            InteriorFogConfig.Logger.LogInfo("Random fog thinness value: " + randomThinness);
+            __instance.indoorFog.parameters.meanFreePath = randomThinness;
+        }
+        else
+        {
+            __instance.indoorFog.parameters.meanFreePath = InteriorFogConfig.fogThinness.Value;
+        }
+        
+        if (InteriorFogConfig.changeFogColor.Value)
+        {
+            if (!ColorUtility.TryParseHtmlString(InteriorFogConfig.fogColor.Value, out Color color))
+            {
+                return;
+            }
+            __instance.indoorFog.parameters.albedo = color;
+        }
+        InteriorFogConfig.Logger.LogInfo("Indoor fog changes done.");
     }
 }
